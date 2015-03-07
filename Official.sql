cfea4453b548ce8669ca3ea0e924b53c5d3f68d9
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-03-07 16:54:32 -0500

    Added more directory structure.

--- Official.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE OFFICIAL 
-(
-	officialID int NOT NULL, 
-	role Varchar (20), 
-	salary int CHECK (Salary > 0),
-	personID int REFERENCES Person (personID),
-	Primary Key (officialID)
-); 
-
