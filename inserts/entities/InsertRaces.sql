0085195bf29d9f36e649762539bfa236e763df9e
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-03-09 17:59:14 -0400

    Changed field in racer.

--- inserts/entities/InsertRaces.sql
+++ inserts/entities/InsertRaces.sql
@@ -1,14 +1,14 @@
-INSERT INTO Race(raceID, name, type, date, gender, mountainName)
+INSERT INTO Race(raceID, name, type, raceDate, gender, mountainName)
 VALUES(12345, 'FIS', 'GS', '2014-12-23', 'M', 'Burke');
 
-INSERT INTO Race(raceID, name, type, date, gender, mountainName)
+INSERT INTO Race(raceID, name, type, raceDate, gender, mountainName)
 VALUES(23456, 'FIS', 'GS', '2015-01-10', 'F', 'Burke');
 
-INSERT INTO Race(raceID, name, type, date, gender, mountainName)
+INSERT INTO Race(raceID, name, type, raceDate, gender, mountainName)
 VALUES(34567, 'USSA', 'SL', '2015-02-06', 'M', 'Nashoba');
 
-INSERT INTO Race(raceID, name, type, date, gender, mountainName)
+INSERT INTO Race(raceID, name, type, raceDate, gender, mountainName)
 VALUES(45678, 'USSA', 'GS', '2014-03-23', 'F', 'Nashoba');
 
-INSERT INTO Race(raceID, name, type, date, gender, mountainName)
+INSERT INTO Race(raceID, name, type, raceDate, gender, mountainName)
 VALUES(56789, 'FIS', 'GS', '2014-02-18', 'F', 'Burke');
