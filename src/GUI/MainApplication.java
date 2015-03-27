d47360dbb72747f0ab8d9b2ef8a80dc3f7eb3ae6
Emily Sager <emilysager@vpn125050.wireless.mcgill.ca>
2015-03-27 19:02:08 -0400

    Front-end completed

---
+++ src/GUI/MainApplication.java
@@ -0,0 +1,26 @@
+package GUI;
+
+public class MainApplication {
+
+	public static void main(String [] args)
+	{ 
+		OptionMenu Menu = new OptionMenu(); 
+
+		while(!Menu.doesUserWantsToquit())
+		{
+			Menu.displayOptions();
+			String lSelectedOption = Menu.receiveUserInput(); 
+			if (!(lSelectedOption.equals("1")||lSelectedOption.equals("2") || lSelectedOption.equals("3") || lSelectedOption.equals("4") || 
+					lSelectedOption.equals("5")))
+			{
+				System.out.println("Please enter a valid option number 1-5."); 
+			}
+			else
+			{
+				Menu.performOption(lSelectedOption);
+			}
+
+		}
+	}
+
+}
